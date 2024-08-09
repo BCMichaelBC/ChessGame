3c4913c3a59e231a1a83df8944ff0ff50552952d
Michael Otasowie <michaelotasowie@gmail.com>
2024-08-09 12:35:30 -0400

    Made it so pawn can now intereact and and capture other pawn and other peices currently the only piece that can move. need to fix promotion

--- main.js
+++ main.js
@@ -80,6 +80,8 @@ function drag(act)
     if((isWhiteTurn && pieceColor == "white") || (!isWhiteTurn && pieceColor == "black"))
     {
         act.dataTransfer.setData("text", piece.id);
+        const startSqID = piece.parentNode.id;
+        getPossibleMoves(startSqID, piece);
     }
 
 }
@@ -94,16 +96,18 @@ function drop(act)
     const destSquare = act.currentTarget;
     let destSquareId = destSquare.id;
 
-    if(isSquareTaken(destSquare) == "blank")
+    // legalSquares.includes() we do this to only allow a piece to dropped when the square id is among the legal ids
+    if((isSquareTaken(destSquare) == "blank") && (legalSquares.includes(destSquareId)))
     {
         console.log(isSquareTaken(destSquare));
         destSquare.appendChild(piece);
         isWhiteTurn = !isWhiteTurn;
+        legalSquares.length = 0;
         return
     }
 
 // if the square is taken remove all of its childern before appending, should look like a capture.
-    if(isSquareTaken(destSquare) != "blank")
+    if(isSquareTaken(destSquare) != "blank" && legalSquares.includes(destSquareId))
     {
         console.log(isSquareTaken(destSquare));
         while(destSquare.firstChild)
@@ -112,6 +116,7 @@ function drop(act)
         }
         destSquare.appendChild(piece);
         isWhiteTurn = !isWhiteTurn;
+        legalSquares.length = 0;
         return;
 
     }
@@ -139,6 +144,108 @@ function isSquareTaken(square)
     {
         return "blank";
     }
+
+
+}
+
+/* 
+* Finding the lega moves for pieces is to determin the legal moves for 
+* each peice
+*/
+function getPossibleMoves(startSqID, piece)
+{
+    const pieceColor = piece.getAttribute("color");
+    if(piece.classList.contains("pawn"))
+    {
+        getPawnMoves(startSqID, pieceColor);
+    }
+}
+
+function getPawnMoves(startSqID, pieceColor)
+{
+// pawn can capture diagonally
+    checkPawnDiagCap(startSqID, pieceColor);
+    checkPawnMoves(startSqID, pieceColor);
+}
+
+
+/*
+* @checkPawnDiag function checks the two diagonal squares
+* on the top left and right of the pawn. If there are pieces of the opposite
+* color on those squares it adds them to the legalSquare array allowing you to capture
+*/
+function checkPawnDiagCap(startSqID, pieceColor)
+{
+    const file = startSqID.charAt(0);
+    const rank = startSqID.charAt(1);
+    const rankNum = parseInt(rank);
+    let currentFile = file;
+    let currentRank = rankNum;
+    let currentSquareID = currentFile + currentRank;
+    let currentSquare = document.getElementById(currentSquareID);
+    let SquareContent = isSquareTaken(currentSquare);
+    const direction = pieceColor == "white" ? 1: -1;
+
+
+
+    currentRank += direction;
+
+    for(let i = -1; i <= 1; i+=2)
+    {
+        currentFile = String.fromCharCode(file.charCodeAt(0) + i);
+        if(currentFile >= "a" && currentFile <= "h")
+        {
+            currentSquareID = currentFile+currentRank;
+            currentSquare = document.getElementById(currentSquareID);
+            SquareContent = isSquareTaken(currentSquare);
+
+    // if a square is occupied by a piece of the opposite color, then the pawn can capture it 
+    // thus the id should be added to the legal squares.
+
+            if(SquareContent!= "blank" && SquareContent != pieceColor)
+            {
+                legalSquares.push(currentSquareID);
+            }
+        }
+    }
+
+
+
+
+}
+function checkPawnMoves(startSqID, pieceColor)
+{
+    const file = startSqID.charAt(0);
+    const rank = startSqID.charAt(1);
+    const rankNum = parseInt(rank);
+    let currentFile = file;
+    let currentRank = rankNum;
+    let currentSquareID = currentFile + currentRank;
+    let currentSquare = document.getElementById(currentSquareID);
+    let SquareContent = isSquareTaken(currentSquare);
+    const direction = pieceColor == "white" ? 1: -1;
+
+    currentRank += direction;
+
+    currentSquareID = currentFile+currentRank;
+    currentSquare = document.getElementById(currentSquareID);
+    SquareContent = isSquareTaken(currentSquare);
+    if(SquareContent!= "blank" )
+    {
+        // if its not blank then there are no legal moves
+        return;
+    }
+    legalSquares.push(currentSquareID);
+
+    if(rankNum != 2 && rankNum != 7)
+    {
+        return;
+    }
+    currentRank += direction;
+    currentSquareID = currentFile+currentRank;
+    currentSquare = document.getElementById(currentSquareID);
+    SquareContent = isSquareTaken(currentSquare);
+    legalSquares.push(currentSquareID);
 
 
 }
