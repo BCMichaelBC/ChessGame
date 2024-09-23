b919890d23758d1250b5df8da0621a792f4e9d18
Michael Otasowie <michaelotasowie@gmail.com>
2024-09-23 12:54:29 -0400

    running into erros with the sharedarrayBuffer

--- main.js
+++ main.js
@@ -38,6 +38,10 @@ let postitionArray = [];
 setupBoard();
 setupPieces();
 fillBoardSquaresArray();
+let startingPosition = generateFEN(boardSquaresArray);
+getEvaluation(startingPosition, function(evaluations){
+    displayEvaluation(evaluations)
+})
 
 
 function makeMove(startSqID, destSquareId, pieceType, pieceColor, captured, promotedTo = "blank")
@@ -1286,6 +1290,10 @@ function checkForEndGame()
 {
     checkForCheckMateAndStaleMate();
     let currentPosition = generateFEN(boardSquaresArray);
+    getEvaluation(currentPosition, function(evaluations)
+{
+    displayEvaluation(evaluations);
+});
     postitionArray.push(currentPosition);
     let insufficientMaterial = hasInsufficientMaterial(currentPosition);
     let threeFoldRepetition = isThreefoldRepetition();
@@ -1435,7 +1443,58 @@ function getAllPossibleMoves(squaresArray, pieceColor)
         return legalSquares;
     })
 }
+function getEvaluation(fen,callback) {
+  var engine = new Worker("./node_modules/stockfish/src/stockfish-nnue-16-no-Worker.js");
+  let evaluations =[];
 
+  engine.onmessage = function (event) {
+    let message = event.data;
+    console.log(message);
+    if(message.startsWith("info depth 10")) {
+      let multipvIndex = message.indexOf("multipv");
+      if(multipvIndex!==-1) {
+        let multipvString = message.slice(multipvIndex).split(" ")[1];
+        let multipv = parseInt(multipvString);
+        let scoreIndex = message.indexOf("score cp");
+        if(scoreIndex!==-1) {
+          let scoreString = message.slice(scoreIndex).split(" ")[2];
+          let evaluation = parseInt(scoreString)/100;
+          evaluation = isWhiteTurn ? evaluation : evaluation * -1;
+          evaluations[multipv-1] = evaluation;
+        } else {
+          scoreIndex = message.indexOf("score mate");
+          scoreString = message.slice(scoreIndex).split(" ")[2];
+          evaluation = parseInt(scoreString);
+          evaluation = Math.abs(evaluation);
+          evaluations[multipv-1] = "#" + evaluation;
+        }
+        let pvIndex = message.indexOf(" pv ");
+        if(pvIndex !== -1) {
+          let pvString = message.slice(pvIndex+4).split(" ");
+          if(evaluations.length===1) {
+            callback(evaluations);
+          }
+        }
+      }
+    }
+  }
+   engine.postMessage("uci");
+   engine.postMessage("isready");
+   engine.postMessage("ucinewgame");
+   engine.postMessage("setoption name multipv value 3");
+   engine.postMessage("position fen "+fen);
+   engine.postMessage("go depth 10");
+}
+
+function displayEvaluation (evaluations) {
+  let blackBar = document.querySelector(".blackBar");
+  let blackBarHeight = 50 - (evaluations[0]/15)*100;
+  blackBarHeight = blackBarHeight>100 ? (blackBarHeight=100) : blackBarHeight;
+  blackBarHeight = blackBarHeight<0 ? (blackBarHeight=0) : blackBarHeight;
+  blackBar.style.height = blackBarHeight + "%";
+  let evalNum = document.querySelector(".evalNum");
+  evalNum.innerHTML = evaluations[0];
+}
 
 function isShortCastlePossble(pieceColor, boardSquaresArray)
 {
